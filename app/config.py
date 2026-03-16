107a8d4ac3d6f509a8257905c16f3e043168dbb9
laurinha <mariakivi@proton.me>
2026-03-16 17:24:27 -0400

    ajustes na condig de conexão com o banco

--- app/config.py
+++ app/config.py
@@ -1,5 +1,16 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://fafnir_user:Abc123@127.0.0.1:3308/fafnir_db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+import os
 
-    CATEGORIES_PER_PAGE = 1
+DB_USER = os.getenv("MYSQL_USER", "root")
+DB_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+DB_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")  # localhost da máquina
+DB_PORT = os.getenv("MYSQL_PORT", "3307")       # porta mapeada no compose
+DB_NAME = os.getenv("MYSQL_DATABASE", "fafnir_db")
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    FLASK_ENV = os.environ.get("FLASK_ENV", "production")
+
+    CATEGORIES_PER_PAGE = 10
+    LOCATIONS_PER_PAGE = 1
